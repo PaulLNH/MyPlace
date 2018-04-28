6338bff484736294f8d321d8f7064605f03e7f92
Christian Dawley <christian@art-box.us>
2018-04-27 22:14:08 -0400

    added ajax call. console logs lat/lng of first result

--- geocoder.js
+++ geocoder.js
@@ -1,28 +1,64 @@
-// Example Outlining Usage of Open-Cage Data's Geocoder
+/* Example Outlining Usage of Open-Cage Data's Geocoder
 
-// API Docs & Other Info Available at:
-// https://geocoder.opencagedata.com/api
+  API Docs & Other Info Available at:
+  https://geocoder.opencagedata.com/api
+*/
 
 // api-key (christian@art-box.us)
 // f64262866250468baf60eec7603c91b8
 
-// Reverse:
+// Reverse geocoding (coords to address):
 //    https://api.opencagedata.com/geocode/v1/json?q=LAT+LNG&key=f64262866250468baf60eec7603c91b8
-// Forward geocoding:
+// Forward geocoding (address to coords):
 //    https://api.opencagedata.com/geocode/v1/json?q=PLACENAME&key=f64262866250468baf60eec7603c91b8
 //
 
-const urlAPI = "https://api.opencagedata.com/geocode/v1/json?";
+// Google Maps Geocoder API key (for possible future reference)
+// AIzaSyCUhGPwwNopLEvA1oz1ptnP-GeA8WO6DEw
 
-  apiKeyOCDGeo = "f64262866250468baf60eec7603c91b8";
 
-function parameters(query, fromCoord) {
-  let parameters = urlAPI + "json?" +
-  srchQuery: query, //address or coordinates (Note: it will be much easier to verify that coordinates are formatted correctly)
-  fromCoord: isCoord, // Boolean value; are you using coordinates instead of a GPS?
-  apiKey: apiKeyGeoCoder;
-}
-return parameters;
+apiKeyGeoCoder = "f64262866250468baf60eec7603c91b8";
+
+const urlAPI = "https://api.opencagedata.com/geocode/v1/";
+
+//searchParams() returns an object to be used with jQuery's .param() method
+
+// query is GPS coordinates or physical address,
+// fromCoord arg. is meant to be a boolean value to check whether or not the search is from GPS coordinates
+
+function searchParams(query, fromCoord) {
+let objPrm = {
+q: query,
+key: apiKeyGeoCoder,
+};
+return objPrm;
 };
 
-console.log(parameters("420 Shakedown Street", true));
+// let thisSearch = searchParams("420 Shakedown Street", true);
+//queryURL = urlAPI + "json?" + thisSearch.JSONs;
+
+/*
+
+        // Creates AJAX call for the specific movie button being clicked
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+
+          /*
+            moviePoster = $("#movie-poster");
+            movieData = $("#movie-data");
+            moviePoster.empty();
+            movieData.empty();
+
+            movTitle = response.Title;
+            movYear = response.Year;
+            movRated = response.Rated;
+            movPlot = response.Plot;
+            movPoster = response.Poster;
+
+            displayData();
+
+
+        });  */
