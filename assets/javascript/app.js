2d19d6d98ba5f3e52f0dc7846bc2c1b6e6aafce9
fitharwe <jesterenwalker@gmail.com>
2018-04-28 11:32:44 -0400

    add js link

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,3 +9,19 @@ $(".heart.fa").click(function() {
   $(this).toggleClass("fa-heart fa-heart-o");
 });
 
+$(document).ready(function () {
+  $('.carousel').carousel({
+    interval: false
+  });
+
+  // manual carousel controls
+  $('.next').click(function () {
+      $('.carousel').carousel('next');
+      return false;
+  });
+  $('.prev').click(function () {
+      $('.carousel').carousel('prev');
+      return false;
+  });
+
+})(jQuery);
